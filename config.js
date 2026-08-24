14dc0cf39bf69f13eb9d17d639bf48992c71e74d
semoljeh <aromtemplate@gmail.com>
2026-08-24 21:51:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbz2RhQCLOGg85zcdoU3B-pvFU-I39ABwkFJB5Oln8hCthc4UwafcMsVx2dBCsH31sBk/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyYiqy2YPlfW9J-Cfy_Aj65QJyYmkGGQRe_EsRrvnRhDWsJyLWysUUf9qyeTUuDjD4_/exec';
