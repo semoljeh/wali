c31053a525cc817cda4d9b6489d756a7179df4e3
semoljeh <aromtemplate@gmail.com>
2026-09-02 08:45:27 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxRfbTluHyFmpupMU-zezEiD8QZTQZf-7cwlippkJZV1ab-35Xxjf1hhfqI5wyZZrIW/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxjXDfC8yE81TpekiXKL474qRgRaufuUt5PztUEFaGYFvQ_QsHfeYgHYtbvSeTy8PX2/exec';
